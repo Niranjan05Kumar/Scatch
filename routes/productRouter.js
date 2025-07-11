833ad06793c2a285a368f796f473eddc71a6c201
Niranjan Kumar <niranjankumar112005@hotmail.com>
2025-07-11 10:01:23 +0530

    isLoggedIn correct

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -2,12 +2,12 @@ const express = require("express");
 const router = express.Router();
 const upload = require("../config/multer-config");
 const { createProduct, deleteProduct, removeFromCart } = require("../controllers/productsController");
-const isloggedIn = require("../middlewares/isloggedIn");
+const isLoggedIn = require("../middlewares/isLoggedIn");
 
 router.post("/create", upload.single("image"), createProduct);
 
 router.get("/delete/:productId", deleteProduct);
 
-router.get("/remove/:productId", isloggedIn, removeFromCart);
+router.get("/remove/:productId", isLoggedIn, removeFromCart);
 
 module.exports = router;
